e5b3ecca378ac05c49c0575bc1087dc6098a3719
Tibo Colman <tibocolman@gmail.com>
2019-10-16 16:17:23 +0200

    error debugging

---
+++ scss/main.js
@@ -0,0 +1,9 @@
+function change_theme_red() {
+    let element = document.getElementById("body");
+    element.classList.add("red");
+} 
+
+function change_theme_blue() {
+    let element = document.getElementById("body");
+    element.classList.remove("red");
+    } 
